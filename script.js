45a713e0991c3464ce141bca261d4a14a6f35d97
rlffudem <rlffudem@gmail.com>
2024-12-24 10:58:10 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// slick.js
+$(function(){
+ $('.visual .slide').slick({
+    arrows: false,
+    dots:true,
+    autoplay: true,
+    fade:true,
+    autoplaySpeed:5000,
+    pauseOnHover:false,
+    PauseOnFocus:false
+ });
+});
